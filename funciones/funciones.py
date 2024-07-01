17ba9985e51e7c6f96c662fba4b179c31fe6a8a0
Ariel Almiron <Danteariel33@gmail.com>
2024-07-01 01:21:13 -0300

    cifrado cesar correcto

--- funciones/funciones.py
+++ funciones/funciones.py
@@ -1,72 +1,56 @@
 import re
 from tda.tda_pilas import Pila, apilar, desapilar, pila_vacia
 
+# Primer paso: Función para invertir los caracteres de cada palabra
 def invertir_letra_cada_palabra(texto_original):
     palabras = texto_original.split(' ')
-    palabras_invertidas = []
+    caracteres_invertidos = []
 
     for palabra in palabras:
         pila = Pila()
         for caracter in palabra:
             apilar(pila, caracter)
         
-        palabra_invertida = ''
+        caracter_invertido = ''
         while not pila_vacia(pila):
-            palabra_invertida += desapilar(pila)
+            caracter_invertido += desapilar(pila)
         
-        palabras_invertidas.append(palabra_invertida)
+        caracteres_invertidos.append(caracter_invertido)
 
-    return ' '.join(palabras_invertidas)
+    return ' '.join(caracteres_invertidos)
 
-
-# Invierte el orden de las palabras
-def invertir_palabra(palabras_invertidas):
-    palabra = re.split(r'(\s+)', palabras_invertidas)
+# Segundo paso: Función para invertir el orden de las palabras
+def invertir_orden_palabras(texto_original):
+    palabras = texto_original.split(' ')
     pila = Pila()
     
-    for caracter in palabra:
-        apilar(pila, caracter)
+    for palabra in palabras:
+        apilar(pila, palabra)
     
-    palabra_invertida = ""
+    palabras_invertidas = ''
     while not pila_vacia(pila):
-        palabra_invertida += desapilar(pila)
+        palabras_invertidas += desapilar(pila) + ' '
     
-    return palabra_invertida
+    return palabras_invertidas.strip()
+
+def cifrado_cesar(texto, desplazamiento):
+    texto_cifrado = ''
+    for caracter in texto:
+        if 32 <= ord(caracter) <= 126:  # Considerar solo caracteres imprimibles
+            nuevo_orden = (ord(caracter) - 32 + desplazamiento) % (126 - 32 + 1) + 32
+            texto_cifrado += chr(nuevo_orden)
+        else:
+            texto_cifrado += caracter  # Dejar los caracteres no imprimibles sin cambios
+    return texto_cifrado
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# def invertir_orden_palabras(texto):
-#     palabras = texto.split()
-#     pila = Pila()
-    
-#     for palabra in palabras:
-#         apilar(pila, palabra)
-    
-#     texto_invertido = ""
-#     while not pila_vacia(pila):
-#         texto_invertido += desapilar(pila) + " "
-    
-#     return texto_invertido.strip()
-
-# def invertir_texto(texto):
-#     cola = Cola()
-#     for caracter in texto:
-#         agregar(cola, caracter)
-    
-#     texto_invertido = ""
-#     while not cola_vacia(cola):
-#         texto_invertido = sacar(cola) + texto_invertido
-    
-#     return texto_invertido
+# Cuarto paso: Función para invertir el texto completamente
+def invertir_texto_completo(texto):
+    pila = Pila()
+    for caracter in texto:
+        apilar(pila, caracter)
+    texto_invertido = ''
+    while not pila_vacia(pila):
+        texto_invertido += desapilar(pila)
+    return texto_invertido
--- main.py
+++ main.py
@@ -9,11 +9,58 @@ Nota: tener en cuenta la cantidad de caracteres del alfabeto y en el caso de dar
 4) Invertir el texto completo
 Nota final ejercicio: aplicar la TAD de pilas y se puede usar la del trabajo anterior"""
 
-from funciones.funciones import invertir_letra_cada_palabra
+from funciones.funciones import invertir_letra_cada_palabra, invertir_orden_palabras, invertir_texto_completo, cifrado_cesar
 
+# Función para encriptar el texto
+def encriptar(texto_original, clave):
+    desplazamiento = clave % 26
+    if desplazamiento == 0:
+        desplazamiento = 3
+
+    # 1. Invertir cada palabra del texto
+    resultado_invertir_letras = invertir_letra_cada_palabra(texto_original)
+    
+    # 2. Invertir el orden de las palabras del texto
+    resultado_invertir_orden = invertir_orden_palabras(resultado_invertir_letras)
+    
+    # 3. Aplicar el cifrado César al texto completo
+    
+    resultado_cifrado = cifrado_cesar(resultado_invertir_orden, desplazamiento)
+    
+    # 4. Invertir el texto completo
+    resultado_final = invertir_texto_completo(resultado_cifrado)
+    
+    return resultado_final
+
+# Función para descifrar el texto
+def descifrar(texto_cifrado, clave):
+    desplazamiento = clave % 26
+    if desplazamiento == 0:
+        desplazamiento = 3
+    desplazamiento = -desplazamiento  # Invertir el desplazamiento para descifrar
+
+    # 1. Invertir el texto completamente
+    resultado_invertido = invertir_texto_completo(texto_cifrado)
+    
+    # 2. Aplicar el descifrado César al texto completo
+    resultado_descifrado = cifrado_cesar(resultado_invertido, desplazamiento)
+    
+    # 3. Invertir el orden de las palabras del texto
+    resultado_ordenado = invertir_orden_palabras(resultado_descifrado)
+    
+    # 4. Invertir cada palabra del texto
+    resultado_final = invertir_letra_cada_palabra(resultado_ordenado)
+    
+    return resultado_final
+
+# Solicitar el texto original y la clave del usuario
 texto_original = input('Ingrese un texto: ')
-clave = int(input('Ingrese su clave de seis digitos: '))
+clave = int(input('Ingrese su clave de seis dígitos: '))
 
-resultado = invertir_letra_cada_palabra(texto_original)
+# Encriptar el texto
+texto_encriptado = encriptar(texto_original, clave)
+print(f'Texto encriptado: {texto_encriptado}')
 
-print(resultado)
+# Desencriptar el texto
+texto_desencriptado = descifrar(texto_encriptado, clave)
+print(f'Texto desencriptado: {texto_desencriptado}')
